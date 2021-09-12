7e904978e64a0026e19cc6ffd372f9054a163199
cfcmsm2 <89998201+cfcmsm2@users.noreply.github.com>
2021-09-12 15:19:13 -0600

    More stats

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -28,6 +28,20 @@
 <script>
 import Vue from "vue";
 
+const PAGE_IDS = [
+  "112076063796073",
+  "102540111429955",
+  "100190701670078",
+  "101166864903191",
+  "104996111183539",
+  "102792574600844",
+  "104575054554957",
+  "109859227352531",
+  "105874321094615",
+  "111660853836193",
+  "112969893700175",
+];
+
 function get(url) {
   return new Promise((resolve) => window.FB.api(url, resolve));
 }
@@ -43,6 +57,9 @@ async function getInsights(token, category, time = "days_28") {
   }
 }
 
+function daysAgo(numDays) {
+  return Date.now() - numDays * 24 * 60 * 60 * 1000;
+}
 const isLocal = (place) => /WY|NE|CO/.test(place);
 
 function accumulateNonLocal(data) {
@@ -55,6 +72,10 @@ function accumulateLocal(data) {
   return Object.entries(data)
     .filter((place) => isLocal(place[0]))
     .reduce((a, b) => a + b[1], 0);
+}
+
+function toPercent(number) {
+  return Math.round(number * 10000) / 100 + "%";
 }
 
 // TODO: Improve performance using API to get multiple metrics at once
@@ -96,9 +117,60 @@ const columns = [
     },
   },
   {
+    name: "Minute View",
+    async getData(token) {
+      return getInsights(token, "page_video_views_organic");
+    },
+  },
+  {
     name: "Engagement Rate",
     async getData(token) {
-      return getInsights(token, "page_post_engagements");
+      const pagePostData = (
+        await get(
+          `/me/feed?fields=insights.metric(post_engaged_users,post_impressions_organic_unique),created_time&access_token=${token}`
+        )
+      ).data.filter((post) => new Date(post.created_time) > daysAgo(28));
+
+      let totalEngagement = pagePostData.reduce(
+        (total, post) =>
+          total +
+          post.insights.data.find(
+            (insight) => insight.name === "post_engaged_users"
+          ).values[0].value,
+        0
+      );
+      let totalReach = pagePostData.reduce(
+        (total, post) =>
+          total +
+          post.insights.data.find(
+            (insight) => insight.name === "post_impressions_organic_unique"
+          ).values[0].value,
+        0
+      );
+
+      if (!totalReach) return "N/A";
+      return toPercent(totalEngagement / totalReach);
+    },
+  },
+  {
+    // post_impressions_fan_unique
+    name: "Followers Reached",
+    async getData(token) {
+      const pagePostData = (
+        await get(
+          `/me/feed?fields=insights.metric(post_impressions_fan_unique),created_time&access_token=${token}`
+        )
+      ).data.filter((post) => new Date(post.created_time) > daysAgo(28));
+
+      const totalFanReach = pagePostData.reduce(
+        (total, post) => total + post.insights.data[0].values[0].value,
+        0
+      );
+      const averageFanReach = totalFanReach / pagePostData.length;
+
+      const likes = await getInsights(token, "page_fans", "");
+
+      return toPercent(averageFanReach / likes);
     },
   },
 ];
@@ -141,11 +213,15 @@ export default {
     },
     async loadData() {
       const { data } = await get(
-        "/me/accounts?fields=access_token,name&limit=100"
+        "/me/accounts?fields=access_token,name,id&limit=100"
       );
 
       await Promise.all(
-        data.map(({ access_token, name }) => {
+        data.map(({ access_token, name, id }) => {
+          if (!PAGE_IDS.includes(id)) {
+            return Promise.resolve();
+          }
+
           Vue.set(this.pageData, name, {});
           return Promise.all(
             columns.map((column) => {
